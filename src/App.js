a36936fd5326a7b3c9d31ac466d5adfe545408ab
Srijani Parui <72404928+srijani07@users.noreply.github.com>
2021-09-19 15:23:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles.css";
 
+var headingText = "srijani's";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1> {headingText}</h1>
     </div>
   );
 }
